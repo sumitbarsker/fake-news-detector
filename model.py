1a126401ec3b2c16be9dd5979df1caa2a95cca85
sumit barsker <sumitbarsker876@gmail.com>
2026-06-18 20:42:01 +0530

    Reformat model.py for improved readability

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression 
 from sklearn.metrics import accuracy_score
 
 # Load dataset
